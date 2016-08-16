566c7a1ecc016a6f1c6b1cf60e0efcfe3394347f
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-08-16 20:15:16 +0900

    上限境界の実装例

---
+++ src/main/scala/UpperBounds.scala
@@ -0,0 +1,31 @@
+class Person(val firstName: String, val lastName: String) extends Ordered[Person] {
+  override def compare(that: Person): Int = {
+    val lastNameComparison = lastName.compareToIgnoreCase(that.lastName)
+    if (lastNameComparison != 0)
+      lastNameComparison
+    else
+      firstName.compareToIgnoreCase(that.firstName)
+  }
+
+  override def toString: String = firstName + " " + lastName
+}
+
+object Util extends App {
+  def orderedMergeSort[T <: Ordered[T]](xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] =
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x :: xs1, y :: ys1) =>
+          if (x < y) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(orderedMergeSort(ys), orderedMergeSort(zs))
+    }
+  }
+}
